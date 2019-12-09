ba0cb8dc0b511d16381a317fe1124c05708c7fc2
3One0Seven <Harry.zhenhao@gmail.com>
2019-12-08 22:06:45 -0500

    deploy

--- squirreltracker/settings.py
+++ squirreltracker/settings.py
@@ -25,20 +25,20 @@ SECRET_KEY = '&4tki@i(qo#8(zir5#bopx49d=@agtd+t-91$-ak&ss@6+q9tg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'map',
+    'sightings.apps.SightingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sightings',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'collected-static'
